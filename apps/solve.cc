06f842c2d453d7cb1d870d1f9b5237f4f00cbf38
connell <hecht3@illinois.edu>
2020-03-20 11:17:33 -0500

    Revert "Attempted to make constructor for sudoku game"
    This reverts commit 56bb3554fff1d669b8f57a630f2c288df1381628.

--- apps/solve.cc
+++ apps/solve.cc
@@ -1,7 +1,6 @@
 // Copyright (c) 2020 [Your Name]. All rights reserved.
 
 #include <sudoku/solver.h>
-#include <sudoku/sudoku_game.h>
 
 int main(int argc, char** argv) {
   return 0;
--- tests/test-sudoku.cc
+++ tests/test-sudoku.cc
@@ -1,14 +1,12 @@
-// Copyright (c) 2020 connell. All rights reserved.
+// Copyright (c) 2020 [Your Name]. All rights reserved.
 
 #define CATCH_CONFIG_MAIN
 
 #include <catch2/catch.hpp>
 #include <sudoku/solver.h>
-#include <sudoku/sudoku_game.h>
 
 
 // TODO(you): Remove this unnecessary test case.
 TEST_CASE("Sanity Check", "[addition]") {
-  sudoku_game* parser = new sudoku_game("data/emptyBoard.spf");
-  REQUIRE(true);
+  REQUIRE(1 + 1 == 2);
 }
