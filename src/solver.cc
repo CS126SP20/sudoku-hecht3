b87ea808107bd6c9b6bef879a72839a5a8a99857
connell <hecht3@illinois.edu>
2020-03-23 17:04:45 -0500

    Wrote Solve method

--- src/solver.cc
+++ src/solver.cc
@@ -10,4 +10,23 @@ class solver {
 
 };
 
+bool sudoku::solver::Solve(char board[kColLength][kRowLength]) {
+  std::vector<int> nextEmptyCell;
+  nextEmptyCell = FindEmptyCell(board);
+  if (nextEmptyCell[0] == -1) {
+    PrintBoard(board);
+    return true;
+  }
+  for (int move = 1; move < 10; move++) {
+    if (CheckValidMove(board, nextEmptyCell[0], nextEmptyCell[1], move)) {
+      board[nextEmptyCell[0]][nextEmptyCell[1]] = static_cast<char>(move);
+      if (Solve(board)) {
+        return true;
+      }
+      board[nextEmptyCell[0]][nextEmptyCell[1]] = '_';
+    }
+  }
+  return false;
+}
+
 
